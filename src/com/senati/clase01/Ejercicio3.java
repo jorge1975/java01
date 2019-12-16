a3215c1bde01303c8672c437cd9f5d488be98e83
Administrador <Administrador@LAB103-PCINSTRUCTOR>
2019-12-16 11:00:19 -0500

    Envio del proyecto al 50%

---
+++ src/com/senati/clase01/Ejercicio3.java
@@ -0,0 +1,16 @@
+package com.senati.clase01;
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Evalúe el siguiente bloque de código y diga qué valor contendrá al final la variable solución 
+		int x=5;
+		int y=8;
+		int z=6;  
+		int solucion= x+y-3*z;
+		System.out.println(solucion);
+
+	}
+
+}
